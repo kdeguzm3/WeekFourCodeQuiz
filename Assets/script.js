34d53a0bf0a8810aaa90f1cf1fffe36518f1f762
Cosplay Diver <kaydeekhing@gmail.com>
2020-02-21 01:15:37 -0500

    Fully functional.  Added highscores and localstorage functions

--- Assets/script.js
+++ Assets/script.js
@@ -3,6 +3,7 @@ let questionInv = [];
 let questionIndex = 0;
 let score = 0;
 let answer;
+let userName;
 let time = questions.length * 15;
 let timer;
 
@@ -12,6 +13,9 @@ let timerDiv = document.querySelector("#timer");
 let choicesDiv = document.querySelector("#choicesBox");
 let startBtn = document.querySelector("#startBtn");
 let highscoreBtn = document.querySelector("#highscoreBtn");
+let modalDiv = document.querySelector("#modal");
+let modalContent = document.querySelector("#modalContent");
+let submit = document.createElement("button");
 
 //sfx
 let sfxRight = new Audio("Assets/sfx/correct.wav");
@@ -72,7 +76,7 @@ function buildQuestion() {
         newChoice.textContent = element;
         choicesDiv.appendChild(newChoice);
     })
-    console.log(choiceArray);
+
 }
 
 startBtn.addEventListener("click", () => {startQuiz();});   
@@ -82,7 +86,6 @@ choicesDiv.addEventListener("click", (e) => {
         if (e.target.getAttribute("data") == answer){
             sfxRight.play();
             score++;
-            console.log("correct");
         } else {
             sfxWrong.play();
             time = time - 15;
@@ -102,4 +105,84 @@ function quizEnd() {
     choicesDiv.innerHTML = "";
     startBtn.classList.remove("hide");
     clearInterval(timer);
+    showResult();
+}
+
+function showResult(){
+    modalContent.innerHTML = "";
+    let congrats = document.createElement("h1");
+        congrats.textContent = "Your score is:";
+        modalContent.appendChild(congrats);
+    let scoreDisplay = document.createElement("h2");
+        scoreDisplay.textContent = score;
+        modalContent.appendChild(scoreDisplay);
+    let label = document.createElement("p");
+        label.textContent = "Please enter your initials:";
+        modalContent.appendChild(label);
+    let textArea = document.createElement("input");
+        textArea.setAttribute("type", "text");
+        textArea.setAttribute("id", "userInitials");
+        textArea.style.color = "black";
+        modalContent.appendChild(textArea);
+        submit.style.cssText = "align-self: center; background-color: blue; width: 150px;";
+        submit.textContent = "Submit";
+        modalContent.appendChild(submit);
+    modalDiv.style.display = "flex";
+}
+
+modalDiv.addEventListener("click", (e) => {
+    if (e.target == modalDiv){
+        modalDiv.style.display = "none";
+    }
+});
+
+highscoreBtn.addEventListener("click", (e) => {
+    showScores();
+})
+
+submit.addEventListener("click", () => {
+    userName = document.querySelector("#userInitials").value.trim();
+    if (userName != "") {
+        storeScore();
+        showScores();
+    }
+})
+
+function storeScore() {
+    let highscores = window.localStorage.getItem("highscores");
+    if (highscores == null) {
+        highscores = [];
+        }
+        else {
+            highscores = JSON.parse(highscores);
+        }
+    let newScore = {"name": userName, "score": score};
+    highscores.push(newScore);
+    highscores.sort((a, b) => {return b.score - a.score});
+    window.localStorage.setItem("highscores", JSON.stringify(highscores));
+}
+
+function showScores() {
+    modalContent.innerHTML = "";
+    let x;
+    let title = document.createElement("h1");
+        title.textContent = "Top 5";
+        modalContent.appendChild(title);
+    let scoreList = document.createElement("ul");
+        scoreList.style.listStyle = "none";
+        modalContent.appendChild(scoreList);
+    let highscores = JSON.parse(window.localStorage.getItem("highscores"));
+    
+    if (highscores.length <= 5) {
+         x = highscores.length;
+    } else {
+         x = 5;
+    }
+    let newScore;
+    for (let y = 0; y < x; y++) {
+        newScore = document.createElement("li");
+        newScore.textContent = "Name: " + highscores[y].name + "| score: " + highscores[y].score;
+        scoreList.appendChild(newScore);
+    }
+    modalDiv.style.display = "flex";
 }
